7c5b9dca057ffc14d7079cf7195e4f751e2d855c
eliza8855 <elopezvalerin@gmail.com>
2021-07-06 22:01:38 -0500

    creacion de filtro por nombre

--- src/data.js
+++ src/data.js
@@ -1,9 +1,18 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+import data from './data/pokemon/pokemon.js';
+const pokemonList = data.pokemon;
+const searchByName = document.querySelector('#inputFilterByName');
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+export const filterByName = () => {
+  console.log(searchByName.value);
+  let prueba = pokemonList.filter((x) => {
+    return (x.name.startsWith(searchByName.value.toLowerCase()));
+  });
+  console.log(prueba);
+}
+
+
+//export const anotherExample = () => {
+// return 'OMG';
+//};
