aa03e3ead4e1f133658b2a88ea845f0e70ce8b36
eliza8855 <elopezvalerin@gmail.com>
2021-07-20 17:37:34 -0500

    creación de tablas para modal

--- src/main.js
+++ src/main.js
@@ -183,7 +183,17 @@ const showPokemon = (list) => {
                                 <td colspan="6"><b>Weaknesses:</b></td>
                               </tr>
                             </table>
-                            
+                             
+                              <div class="grid">
+                                 <div class="grid1">1</div>
+                                 <div class="grid2">2</div>
+                                 <div class="grid3">3</div>
+                                 <div class="grid4">4</div>
+                                 <div class="grid5">5</div>
+                                 <div class="grid6">6</div>
+                                 <div class="grid7">7</div>
+                              </div>
+
                           </div>
                           <div class="modal-info">
                             
